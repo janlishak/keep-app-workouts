230ee9364dd5b7e008175724d3ffab78ffe85f9a
Jan Lishak <35602700+janlishak@users.noreply.github.com>
2021-05-09 01:06:05 +0200

    donze for today

--- app/src/main/java/com/janlishak/keepappworkouts/MainActivity.java
+++ app/src/main/java/com/janlishak/keepappworkouts/MainActivity.java
@@ -2,8 +2,11 @@ package com.janlishak.keepappworkouts;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,27 +17,33 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        //Navigation boilerplate
-//        BottomNavigationView navView = findViewById(R.id.nav_view);
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
+        //Navigation boilerplate
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
 //        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
 //                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
 //                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(navView, navController);
-//        /////////////////
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+        /////////////////
 
         toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(v -> {
+            Snackbar.make(v, "hey", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        });
 
     }
 
